34c798ba30ca992adce3407c873469753bf073b0
Ilovealtapow <Jfloyd421@gmail.com>
2022-01-03 14:41:27 -0700

    save

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
     return (
         <div className= "header">
             <div className="headerTitles">
-                <span className="headerTitleSm">Jordan Floyd</span>
-                <span className="headerTitleLg">Blog</span>
+                <span className="headerTitleLg">Jordan Floyd</span>
+                <span className="headerTitleLg"></span>
             </div>
             <img className="headerImg"
              src="https://res.cloudinary.com/dvavaunrm/image/upload/v1640977911/20201106_164447_p9g5ao.jpg"
