d9d50c3b66dd002a8f54d4f50e3e0c188ae7f19d
Ilovealtapow <Jfloyd421@gmail.com>
2022-01-05 14:27:36 -0700

    login

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -25,7 +25,18 @@ router.post("/register", async(req,res) =>{
 
 
 //Login
+router.post("/login", async (req,res) =>{
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("Credentials incorrect!")
 
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Credentials incorrect!")
+    }catch (err) {
+        res.status(500).json(err);
+
+    }
+})
 
 
 module.exports = router
